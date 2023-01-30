c0b7c8b20ee74349a883471b314fb990b23e318a
Isabelnps <isabelnps@Isabelnpss-MBP.realtek>
2023-01-30 12:39:04 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to my
+          Welcome to my Project
         </p>
         <a
           className="App-link"
